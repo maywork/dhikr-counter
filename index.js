5f62323bb3c5b2917414c5d320fbe5bf74f881d4
Mayesha Fairuz <69720234+maywork@users.noreply.github.com>
2025-04-28 19:12:52 +0600

    Added source files

---
+++ index.js
@@ -0,0 +1,19 @@
+let saveEl = document.getElementById("save-el")
+let countEl = document.getElementById("count-el")
+let totalEl = document.getElementById("total-el")
+let count = 0
+let total = 0
+
+function increment() {
+    count += 1
+    countEl.textContent = count
+}
+
+function save() {
+    let countStr = count + " - "
+    saveEl.textContent += countStr
+    total += count
+    totalEl.textContent = "Total entry: " + total
+    countEl.textContent = 0
+    count = 0
+}
